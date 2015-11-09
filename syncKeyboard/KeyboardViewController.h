22a16aa576292e1b030642e01141d26eecd901e4
caa1211 <caa@livemail.tw>
2015-11-09 21:25:57 +0800

    init

---
+++ syncKeyboard/KeyboardViewController.h
@@ -0,0 +1,13 @@
+//
+//  KeyboardViewController.h
+//  syncKeyboard
+//
+//  Created by caa on 11/9/15.
+//  Copyright © 2015 yahoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KeyboardViewController : UIInputViewController
+
+@end
