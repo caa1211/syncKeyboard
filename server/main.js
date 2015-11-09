1286738a907f148f2cfe17bb5916eddfb7153699
Carter Chang <carter@yahoo-inc.com>
2015-11-10 02:11:39 +0800

    finish prototype

--- server/main.js
+++ server/main.js
@@ -1,8 +1,8 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var fs = require("fs");
-var socketio = require('socket.io');
-var io = socketio.listen(http);
+var io = require('socket.io').listen(3001);
+//var io = socketio.listen(http);
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/webClient/index.html');
@@ -12,12 +12,36 @@ http.listen(3000, function(){
   console.log('listening on *:3000');
 });
 
-io.on('connection', function(socket){
-  console.log('a user connected');
-  socket.emit('ready');
 
-  socket.on('msg', function (msg) {
-    console.log('msg', msg);
+io.sockets.on('connection', function(socket, aaa){
+
+  console.log('a user connected');
+  socket.on('set nickname', function (name) {
+
+    socket.set('nickname', name, function () { 
+    	console.log("name", name);
+    	socket.emit('ready'); 
+    });
+
   });
+
+  socket.on('message', function (message) {
+  	console.log("message", message);
+   
+  });
+
+
+  socket.on('joinRoom', function (room) {
+  	console.log("join room", room);
+    socket.join(room);
+  });
+
+
+
+  socket.on('sendMsg', function (param) {
+  	console.log("sendMsg to room", param.room);
+    socket.broadcast.to(param.room).emit('receiveMsg', param); 
+  });
+
 });
 
--- syncKeyboard/KeyboardViewController.m
+++ syncKeyboard/KeyboardViewController.m
@@ -7,9 +7,13 @@
 //
 
 #import "KeyboardViewController.h"
+#import <SocketIO.h>
+#import <SocketIOPacket.h>
 
-@interface KeyboardViewController ()
+@interface KeyboardViewController () <SocketIODelegate>
+@property (weak, nonatomic) IBOutlet UILabel *debugLabel;
 @property (nonatomic, strong) UIButton *nextKeyboardButton;
+@property (nonatomic, strong) SocketIO *socketIO;
 @end
 
 @implementation KeyboardViewController
@@ -26,7 +30,7 @@
     // Perform custom UI setup here
     self.nextKeyboardButton = [UIButton buttonWithType:UIButtonTypeSystem];
     
-    [self.nextKeyboardButton setTitle:NSLocalizedString(@"Next Keyboard", @"Title for 'Next Keyboard' button") forState:UIControlStateNormal];
+    [self.nextKeyboardButton setTitle:NSLocalizedString(@"Next 4", @"Title for 'Next Keyboard' button") forState:UIControlStateNormal];
     [self.nextKeyboardButton sizeToFit];
     self.nextKeyboardButton.translatesAutoresizingMaskIntoConstraints = NO;
     
@@ -37,6 +41,78 @@
     NSLayoutConstraint *nextKeyboardButtonLeftSideConstraint = [NSLayoutConstraint constraintWithItem:self.nextKeyboardButton attribute:NSLayoutAttributeLeft relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeLeft multiplier:1.0 constant:0.0];
     NSLayoutConstraint *nextKeyboardButtonBottomConstraint = [NSLayoutConstraint constraintWithItem:self.nextKeyboardButton attribute:NSLayoutAttributeBottom relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeBottom multiplier:1.0 constant:0.0];
     [self.view addConstraints:@[nextKeyboardButtonLeftSideConstraint, nextKeyboardButtonBottomConstraint]];
+    
+    [self setupSocketio];
+}
+
+- (void) setupSocketio {
+    NSString *socketServer = @"believeweave.corp.gq1.yahoo.com";
+    self.socketIO = [[SocketIO alloc] initWithDelegate:self];
+    self.socketIO.delegate = self;
+    //[socketIO connectToHost:@"believeweave.corp.gq1.yahoo.com" onPort:3000 withParams:@{@"auth_token":@"1234"}];
+    [self.socketIO connectToHost:socketServer onPort:3001];
+    //self.debugLabel.text = @"00";
+}
+
+- (IBAction)onBtnClick:(id)sender {
+    [self.socketIO sendEvent:@"sendMsg" withData:@{@"room": @"my room", @"data": @"ooo"}];
+}
+
+
+- (void) socketIODidConnect:(SocketIO *)socket{
+    self.debugLabel.text = @"a"; //v
+    
+    [self.socketIO sendEvent:@"joinRoom" withData:@"my room"];
+}
+- (void) socketIODidDisconnect:(SocketIO *)socket disconnectedWithError:(NSError *)error{
+    self.debugLabel.text = @"b";
+}
+
+- (void) socketIO:(SocketIO *)socket didReceiveJSON:(SocketIOPacket *)packet{
+    self.debugLabel.text = @"c";
+}
+
+- (void) socketIO:(SocketIO *)socket didSendMessage:(SocketIOPacket *)packet{
+    //self.debugLabel.text = @"d"; //v
+    
+}
+- (void) socketIO:(SocketIO *)socket onError:(NSError *)error{
+    self.debugLabel.text = @"e"; //v
+}
+
+- (void) socketIO:(SocketIO *)socket didReceiveMessage:(SocketIOPacket *)packet
+{
+    NSLog(@"didReceiveMessage >>> data: %@", packet.data);
+    self.debugLabel.text = @"11";
+}
+
+
+- (NSString *)getLastWordBeforeInput {
+    //get last word before input. if last character is a space, the return value will be an empty string
+    NSString *context = [self.textDocumentProxy documentContextBeforeInput];
+    context = [context stringByReplacingOccurrencesOfString:@"[^a-zA-Z]" withString:@" " options:NSRegularExpressionSearch range:NSMakeRange(0, context.length)];
+    NSArray *listItems = [context componentsSeparatedByString:@" "];
+    if (listItems.lastObject == nil) {
+        return @"";
+    }
+    return listItems.lastObject;
+}
+
+// event delegate
+- (void) socketIO:(SocketIO *)socket didReceiveEvent:(SocketIOPacket *)packet
+{
+    while (self.textDocumentProxy.hasText==YES)
+    {
+        [self.textDocumentProxy deleteBackward];
+    }
+    
+    NSString *remoteInput = packet.args[0][@"data"];
+    self.debugLabel.text = remoteInput;
+    NSLog(@"didReceiveEvent >>> data: %@", packet.data);
+    
+   
+    [self.textDocumentProxy insertText:remoteInput];
+    
 }
 
 - (void)didReceiveMemoryWarning {
